d484c198482c23568f4dc688a87ad399f70fda70
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-05-13 11:47:26 +0200

    Added components

---
+++ src/components/kweets/KweetMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="mx-auto" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">
+          {{ username }}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{ message }}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  props: {
+    username: String,
+    message: String,
+    imageUrl: String,
+  },
+})
+export default class KweetMessage extends Vue {}
+</script>
---
+++ src/components/recipes/CreateRecipe.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Hello there</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class CreateRecipe extends Vue {}
+</script>
